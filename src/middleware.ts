f0aa792fa85129e5683d720c70f9311fe023f83c
guoguotaxianren <81474753+guoguotaxianren@users.noreply.github.com>
2025-10-30 14:11:40 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -133,6 +133,7 @@ function shouldSkipAuth(pathname: string): boolean {
 // 配置middleware匹配规则
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|login|warning|api/login|api/register|api/logout|api/cron|api/server-config).*)',
+    '/((?!_next/static|_next/image|favicon.ico|login|warning|api/login|api/register|api/logout|api/cron|api/server-config|api/tvbox).*)',
   ],
 };
+
